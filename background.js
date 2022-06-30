04126fbe8fb327c527460c9ed12017324b7c64a9
kero13ro <32509189+kero13ro@users.noreply.github.com>
2022-06-30 12:32:47 +0800

    use omnibox

--- background.js
+++ background.js
@@ -1,17 +1,5 @@
-chrome.commands.onCommand.addListener(async (command) => {
-  if (command === "toggle-feature") {
-    // console.log(window);
-    console.log(chrome.tabs);
-    // let url = tabs[0].url;
-
-    chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-      let url = tabs[0].url;
-      console.log(url);
-      // use `url` here inside the callback because it's asynchronous!
-    });
-
-    // chrome.tabs.create({
-    //   url: "https://translate.google.com/?sl=en&tl=zh-CN&text=simulation&op=translate",
-    // });
-  }
+chrome.omnibox.onInputEntered.addListener((text) => {
+  chrome.tabs.create({
+    url: `https://translate.google.com/?sl=en&tl=zh-CN&text=${text}&op=translate`,
+  });
 });
