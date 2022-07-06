070e24c812b355504b385f71c1c7316c1f75d83e
kero13ro <32509189+kero13ro@users.noreply.github.com>
2022-07-06 23:05:04 +0800

    add translaion record

--- omnibox/for_translate/background.js
+++ omnibox/for_translate/background.js
@@ -1,5 +1,12 @@
-chrome.omnibox.onInputEntered.addListener((text) => {
-  chrome.tabs.create({
+chrome.omnibox.onInputEntered.addListener(async (text) => {
+  chrome.tabs.update({
     url: `https://translate.google.com/?sl=en&tl=zh-CN&text=${text}&op=translate`,
+  });
+  chrome.storage.sync.get("wordsList", (data) => {
+    const wordsList = data.wordsList || [];
+
+    chrome.storage.sync.set({ wordsList: [...wordsList, text] }, function () {
+      console.log("Value is set to " + text);
+    });
   });
 });
---
+++ omnibox/for_translate/popup.js
@@ -0,0 +1,66 @@
+const _u = $(".unit");
+const _d = $(".delete");
+_d.remove();
+
+function constructOptions() {
+  $("#root").empty();
+
+  chrome.storage.sync.get("wordsList", (data) => {
+    let wordsList = data.wordsList;
+
+    wordsList.forEach((text, index) => {
+      // const node = _u.clone(true, true).text(text).appendTo("#root");
+
+      const node = _u.clone(true, true);
+      const textWrap = $("<div class='textWrap'></div>");
+      textWrap.text(text).appendTo(node);
+      textWrap.click(() => {
+        chrome.tabs.update({
+          url: `https://translate.google.com/?sl=en&tl=zh-CN&text=${text}&op=translate`,
+        });
+      });
+
+      node.appendTo("#root");
+
+      const _delete = _d.clone(true, true).appendTo(node);
+      _delete.click(() => {
+        wordsList.splice(index, 1);
+        chrome.storage.sync.set({ wordsList });
+        constructOptions();
+      });
+    });
+  });
+}
+
+constructOptions();
+
+// const _u = $(".unit");
+// const _d = $(".delete");
+// _d.remove();
+
+// function constructOptions() {
+//   $("#root").empty();
+
+//   let wordsList = [11, 22, 33];
+
+//   wordsList.forEach((text, index) => {
+//     const node = _u.clone(true, true);
+//     const textWrap = $("<div class='textWrap'></div>");
+//     textWrap.text(text).appendTo(node);
+//     textWrap.click(() => {
+//       chrome.tabs.create({
+//         url: `https://translate.google.com/?sl=en&tl=zh-CN&text=${text}&op=translate`,
+//       });
+//     });
+
+//     node.appendTo("#root");
+//     const _delete = _d.clone(true, true).appendTo(node);
+//     _delete.click(() => {
+//       wordsList.splice(index, 1);
+//       chrome.storage.sync.set({ wordsList });
+//       constructOptions();
+//     });
+//   });
+// }
+
+// constructOptions();
