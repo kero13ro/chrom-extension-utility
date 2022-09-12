db19603e948d7e855a1faf853467059cdbba0ed4
廖庚寅 <ericliao@local>
2022-09-12 23:45:07 +0800

    merge hahow

---
+++ HahowFocus/README.md
@@ -0,0 +1,11 @@
+# hahow-chrome-extension
+
+針對線上課程平台 [hahow](https://hahow.in/)，所開發各類插件功能：<br>
+1. 全螢幕模式，右側可觀看逐字稿、課程總覽。
+2. 點開逐字稿分頁，複製當前影片的逐字稿。
+  
+<br><br>
+## Dev mode
+<hr>
+- chrome://extensions/ <br>
+- 載入未封裝項目
---
+++ HahowFocus/popup.js
@@ -0,0 +1,90 @@
+let copySub = document.getElementById("copySub");
+let focusBtn = document.getElementById("focus");
+
+const extensions = ["https://hahow.in/courses","https://www.udemy.com/"];
+
+
+focusBtn.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  if (!extensions.some(path=>tab.url.startsWith(path))) {
+    return;
+  }
+
+  const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+  const nextState = prevState === "ON" ? "OFF" : "ON";
+  await chrome.action.setBadgeText({
+    tabId: tab.id,
+    text: nextState,
+  });
+
+  if (nextState === "ON") {
+    await chrome.scripting.insertCSS({
+      files: ["focus-mode.css"],
+      target: { tabId: tab.id },
+    });
+  } else if (nextState === "OFF") {
+    await chrome.scripting.removeCSS({
+      files: ["focus-mode.css"],
+      target: { tabId: tab.id },
+    });
+  }
+
+  window.close();
+});
+
+copySub.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  
+  if (!extensions.some(path=>tab.url.startsWith(path))) {
+    return;
+  }
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: handleCopy,
+  });
+
+  window.close();
+});
+
+const handleCopy = () => {
+  const url = document.querySelector("track")?.src;
+
+  const copy = (str) => {
+    const el = document.createElement("textarea");
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+  };
+
+  if (!url) {
+    alert("沒有字幕");
+  }
+  fetch(url)
+    .then((response) => response.text())
+    .then((data) => {
+      const aa = data.split("\n\n").map((item) => item.split("\n")[1]);
+      copy(aa);
+      alert("複製成功！");
+    })
+    .catch((err) => {
+      console.log({ err });
+      alert("沒有字幕");
+    });
+  // const container = document.getElementById("hh-transcript-vtt-container");
+
+  // if (container) {
+  //   let res = "";
+  //   const li = container.querySelectorAll("li");
+  //   li.forEach((ele) => {
+  //     res += `${ele.textContent}\n`;
+  //   });
+  //   copy(res);
+  //   console.log(res);
+  //   alert('複製成功！')
+  // } else {
+  //   alert('沒有字幕')
+  // }
+};
